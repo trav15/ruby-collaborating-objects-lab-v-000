25ffc671962248c7af14f0124f8cd2321ce27db0
Travis T <travistokuyama@gmail.com>
2018-09-26 17:13:02 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,10 +11,15 @@ class Song
     artist, song = filename.split(" - ")
     new_song = self.new(song)
     new_song.artist_name = artist
-    @@all << new_song
-    Artist.add_song(new_song)
+    new_song.save
   end
 
+  def self.find_by_artist(artist)
+    Song.all.select do | song |
+      song.artist == artist
+    end
+  end
+  
   def self.all
     @@all
   end
@@ -23,4 +28,8 @@ class Song
     self.artist = Artist.find_or_create_by_name(name)
   end
   
+  def save
+    @@all << self
+    self
+  end
 end
