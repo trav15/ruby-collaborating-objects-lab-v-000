18bf10657d243428422581375331d886a16569ff
Travis T <travistokuyama@gmail.com>
2018-09-26 16:56:10 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,7 +6,7 @@ class MP3Importer
   end
   
   def files
-    @files ||= Dir.("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+    @files ||= Dir.("#{path}/*.mp3").collect{|f| f.gsub("#{path}/", "") }
   end
   
   def import 
